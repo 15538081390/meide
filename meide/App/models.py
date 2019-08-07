1b49f0f8346f8a96295ac8ef802c5e3d382db280
haohl-keepworking <52267207+haohl-keepworking@users.noreply.github.com>
2019-08-07 21:34:42 +0800

    models

--- meide/App/models.py
+++ meide/App/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class Index(models.Manager):
     sid = models.AutoField(primary_key=True)
-    pid = models.AutoField(null=True)
+    pid = models.IntegerField(null=True)
+    name =models.CharField(max_length=11)
