768023f54f8c5f78fb4f9d5ef888eebd109c3106
Yzz997 <zhiy6@outlook.com>
2019-08-07 21:32:56 +0800

    models

--- meide/App/models.py
+++ meide/App/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Index(models.Manager):
     sabbid = models.AutoField(primary_key=True)
     pid = models.AutoField(null=True)
-
+   
+    def niubi():
+        print('哈哈哈，贼牛逼')
