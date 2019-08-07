c2659401f140ad28e37b7e8a61bdd030fc993a6c
Yzz997 <zhiy6@outlook.com>
2019-08-07 21:28:53 +0800

    models

--- meide/App/models.py
+++ meide/App/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Index(models.Manager):
-    sid = models.AutoField(primary_key=True)
+    sabbid = models.AutoField(primary_key=True)
     pid = models.AutoField(null=True)
+
